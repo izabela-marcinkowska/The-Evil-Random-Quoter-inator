b734ac4295bd3045d86dd1a96e2e5ea29355b4bf
Devy <izabela.marcinkowska@outlook.com>
2024-12-04 15:34:44 +0100

    first commit

---
+++ The Evil Random Quoter-inator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Evil Random Quoter-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Evil Random Quoter-inator/The_Evil_Random_Quoter_inatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Evil_Random_Quoter_inatorApp.swift
+//  The Evil Random Quoter-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct The_Evil_Random_Quoter_inatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
