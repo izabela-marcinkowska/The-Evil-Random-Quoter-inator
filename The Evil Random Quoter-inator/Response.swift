1646fdd87e56c47a83e4cd8903de6183abea5ddd
Devy <izabela.marcinkowska@outlook.com>
2024-12-06 11:49:28 +0100

    feat: Create data structure

---
+++ The Evil Random Quoter-inator/Response.swift
@@ -0,0 +1,16 @@
+//
+//  Response.swift
+//  The Evil Random Quoter-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-04.
+//
+
+import Foundation
+
+
+struct Quote: Codable {
+    var q: String
+    var a: String
+    var c: String?
+    var h: String
+}
